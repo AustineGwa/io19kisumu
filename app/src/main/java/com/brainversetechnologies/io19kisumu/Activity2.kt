1d73b977983d32708b4bf0a3f26ef8170ae12d20
AustineGwa <austinegwa64@gmail.com>
2019-05-18 16:39:22 +0300

    done io app in kotlin. initial commit

---
+++ app/src/main/java/com/brainversetechnologies/io19kisumu/Activity2.kt
@@ -0,0 +1,12 @@
+package com.brainversetechnologies.io19kisumu
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity2 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_2)
+    }
+}
